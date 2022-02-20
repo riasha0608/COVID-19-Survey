031e39d6d4c45c298d6a8f9201856b3b0bfef032
anastasiak36 <akelnik.9@gmail.com>
2022-02-20 15:46:37 -0500

    added randomness to song selection and edited songs

--- survey.js
+++ survey.js
@@ -114,6 +114,18 @@ function getResult() {
         counter += 10;
     }
 
+    var song1 = [];
+    song1[0] = new Audio('music/happy.mp3');
+    song1[1] = new Audio('music/levitating.mp3');
+
+    var song2 = []
+    song2[0] = new Audio('music/on_my_mind.mp3');
+    song2[1] = new Audio('music/mixed_personalities.mp3');
+
+    var song3 = []
+    song3[0] = new Audio('music/oh_my_god.mp3');
+    song3[1] = new Audio('music/heartless.mp3');
+
     if (counter > 97){
         counter = 97;
     }
@@ -124,28 +136,31 @@ function getResult() {
         var h = document.createElement("h2");
         h.innerHTML = "You have a " + counter + "% chance of having COVID";
         document.body.appendChild(h);
+        var index = Math.floor(Math.random() * 2);
         if (counter > 0 && counter < 45) {
             h = document.createElement("h3");
             h.innerHTML = "You most likely don't have COVID, but you could get a rapid test if you would like to make sure :)";
             document.body.appendChild(h);
-            var song = new Audio('music/happy.mp3');
-            song.play();
+            // var song = new Audio('music/happy.mp3');
+            song1[index].play();
         }
         else if (counter >= 45 && counter < 70) {
             h = document.createElement("h3");
             h.innerHTML = "You have a moderate chance of having COVID, we suggest getting a PCR test and being cautious.";
             document.body.appendChild(h);
-            var song = new Audio('music/mixed_personalities.mp3');
-            song.play();
+            // var song = new Audio('music/mixed_personalities.mp3');
+            song2[index].play();
         }
         else if (counter >= 70 && counter <= 97) {
             h = document.createElement("h3");
             h.innerHTML = "You most likely have COVID. Please get both the rapid and PCR test done, and isolate yourself. Feel better!";
             document.body.appendChild(h);
-            var song = new Audio('music/heartless.mp3');
-            song.play();
+            // var song = new Audio('music/heartless.mp3');
+            song3[index].play();
         }
         
         window.scrollTo(0,document.body.scrollHeight);
+        document.body.removeChild(document.getElementById("button"));
+        
     }
 }
